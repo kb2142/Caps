763ad8af629dfa26415af52aaa28e2f330ac11da
Alfred <sxc1450398709@gmail.com>
2019-05-01 17:13:35 -0500

    hh

--- CapsGNN/run.sh
+++ CapsGNN/run.sh
@@ -2,5 +2,4 @@
 T=../dat/pkl/protein_nx2/json/
 P=$T
 O=./output/protein.csv
-M=./cora_pretrained.mod
-python src/main.py --epochs 10 --batch-size 3 --train-graph-folder $T --test-graph-folder $P --prediction-path $O --pretrain $M
+python src/main.py --epochs 10 --batch-size 3 --train-graph-folder $T --test-graph-folder $P --prediction-path $O 
